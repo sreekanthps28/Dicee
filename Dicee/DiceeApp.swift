dc2e9d94d05b6e81a443b8aaf350f584232499ff
Sreekanth <sreekanthps28@gmail.com>
2023-02-28 21:16:22 +0000

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by SREEKANTH PS on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
